36d714d3eceb9ac8580ab92b1c51afff83ee38cf
cg <chris@goebelfamily.com>
2022-01-30 10:49:32 -0500

    working with persistent local blockchain

--- truffle-config.js
+++ truffle-config.js
@@ -8,19 +8,9 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*" //match any network id
-    },
-    rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(
-          privateKeys.split(','), // array of private keys
-          `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}` // Url to an Ethereum node
-        )
-      },
-      gas: 5000000,
-      gasPrice: 25000000000,
-      network_id: 4
+      //network_id: "h75nftftw" //match any network id
     }
   },
   contracts_directory: './src/contracts',
